2882abdda37c7576d73425b8d91e48f03d5d79c9
minhradz <minhradz@gmail.com>
2019-11-07 17:55:04 -0500

    SelectionSort in Java

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class SelectionSort {
+    // method for sorting numbers
+    public static void selectionSort(double[] list){
+        for (int i = 0; i < list.length - 1; i++){
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++){
+                if (currentMin > list[j]){
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            // swap list[i] with list[currentMinIndex] if necessary
+            if (currentMinIndex != i){
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+      double[] array = new double[]{1, 5, 8, 9, 109, 2345, 123, 456467, 5555, 43, 56, 94, 6, 7, 3, 2};
+      selectionSort(array);
+      System.out.println(Arrays.toString(array));
+    }
+}

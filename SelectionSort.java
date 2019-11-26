4fec4353ed53fa6caabaf0045bd5677c79f91364
minhradz <minhradz@gmail.com>
2019-11-26 17:02:49 -0500

    update for SelectionSort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,24 +1,22 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SelectionSort {
     // method for sorting numbers
-    public static void selectionSort(double[] list){
-        for (int i = 0; i < list.length - 1; i++){
-            double currentMin = list[i];
-            int currentMinIndex = i;
+    private static void selectionSort(double[] list){
+        for (int i = 0; i < list.length - 1; i++){ // traverse through every element of the array
+            double currentMin = list[i]; // starting the with first element of the array, it will be the min
+            int currentMinIndex = i; // keeping its index
 
-            for (int j = i + 1; j < list.length; j++){
-                if (currentMin > list[j]){
-                    currentMin = list[j];
-                    currentMinIndex = j;
+            for (int j = i + 1; j < list.length; j++){ // loop through the rest of the array after the i-th element
+                if (currentMin > list[j]){ // if the i-th element is larger than the j-th element
+                    currentMin = list[j]; // the current minimum element is the latter ( j-th ) element
+                    currentMinIndex = j; // the current minimum index is the j-th element ( i-th + 1 )
                 }
             }
-
-            // swap list[i] with list[currentMinIndex] if necessary
+            // swap list[i] with list[currentMinIndex] a.k.a swap the smaller and the larger
             if (currentMinIndex != i){
-                list[currentMinIndex] = list[i];
-                list[i] = currentMin;
+                list[currentMinIndex] = list[i]; // the latter index is swapped with the index before it
+                list[i] = currentMin; //
             }
         }
     }

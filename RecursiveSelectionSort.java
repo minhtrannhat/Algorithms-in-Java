55740956135ceafa22c3848e281727a4c8a44296
minhradz <minhradz@gmail.com>
2020-02-23 21:42:47 +0000

    Adding detailed comments on the overloaded Sort() functions

--- RecursiveSelectionSort.java
+++ RecursiveSelectionSort.java
@@ -1,3 +1,8 @@
+// Two overloaded Sort methods are defined.
+// The first method sort(double[] list), sorts the array in list[0 .. list.length - 1]
+// The second method sort(double[] list, int low, int high) , sorts an array in list[low .. high]
+// The second method can be invoked recursively to sort an ever-shrinking array
+
 import java.util.*;
 
 public class RecursiveSelectionSort {
@@ -31,7 +36,7 @@ public class RecursiveSelectionSort {
     public static void main(String[] args) {
         double[] arr = {5, 6, 1, 4, -6, 0, 8, 7, -5, -6, };
 
-        System.out.println("The current array of integers is:");
+        System.out.println("The current array of doubles is:");
         System.out.println(Arrays.toString(arr));
 
         sort(arr);

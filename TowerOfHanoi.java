6f37c94ea00b277ffd948f11d82d1c0ed356671c
minhradz <minhradz@gmail.com>
2020-02-24 20:11:54 +0000

    Solving the Tower of Hanoi problem using recursion

---
+++ TowerOfHanoi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of disks");
+        int n = input.nextInt();
+
+        System.out.println("The moves are: ");
+        moveDisks(n, 'A', 'B', 'C');
+    }
+
+    public static void moveDisks(int n, char fromTower, char toTower, char auxTower){
+        if(n == 1) // base case
+            System.out.println("Move disk" + n + " from " + fromTower + " to " + toTower);
+
+        else{
+            moveDisks(n - 1, fromTower, auxTower, toTower);
+            System.out.println("Move disk" + n + " from " + fromTower + " to " + toTower);
+            moveDisks(n - 1, auxTower, toTower, fromTower);
+        }
+    }
+}
